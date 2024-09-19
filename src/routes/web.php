63e4fe32f8bf4ff2e6e460d079d5539d8a30be1b
Marco Sala <sala.marco1984@gmail.com>
2024-09-19 14:26:46 +0200

    fix routeing per aggiunta auth alla rotta brewery

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,19 +6,14 @@ use App\Http\Controllers\BeersController;
 use App\Http\Controllers\BreweryController;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/breweries', [BreweryController::class, 'index'])->name('breweries.index');
-
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', [BreweryController::class, 'index'])->name('breweries.index');
+    Route::get('/breweries', [BreweryController::class, 'index'])->name('breweries.index');
     Route::get('/beers', [BeersController::class, 'index'])->name('beers.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
