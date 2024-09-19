c22308e1b2f1351262cc6d24952fd18f8d8c5f6c
Marco Sala <sala.marco1984@gmail.com>
2024-09-19 14:59:41 +0200

    pulizia da componenti non utilizzati, token check

--- src/app/Http/Controllers/BreweryController.php
+++ src/app/Http/Controllers/BreweryController.php
@@ -4,17 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Auth;
+use App\Models\UserToken;
 
 class BreweryController extends Controller
 {
+    public function __construct()
+    {
+        $user = Auth::user();
+
+        // Se l'utente non ha token, blocco l'accesso all'api
+        if (!UserToken::where('user_id', $user->id)->exists()) {
+            Auth::logout();
+            abort(403, 'Accesso negato. Nessun token associato all\'utente.');
+        }
+    }
     public function index(Request $request){
+
         $perPage = config('api.openbrewery.items_per_page', 5);
         $page = $request->input('page', 1);
 
         $response = Http::get(config('api.openbrewery.base_url'));
         $breweries = $response->json();
 
-        $total = count($breweries); // Può essere necessario un altro approccio se l'API fornisce un totale diverso
+        // In assenza di un totale fornito dall'API, cacloclo il totale tramite conteggio
+        $total = count($breweries); 
         $totalPages = ceil($total / $perPage);
 
         $response = Http::get(config('api.openbrewery.base_url'), [
@@ -24,9 +38,6 @@ class BreweryController extends Controller
 
         if ($response->successful()) {
             $breweries = $response->json();
-        
-            // In assenza di un totale fornito dall'API, possiamo calcolare il totale degli elementi
-            $total = count($breweries); // Può essere necessario un altro approccio se l'API fornisce un totale diverso
         
             return view('breweries.index', [
                 'breweries' => $breweries,
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,25 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BeersController;
 use App\Http\Controllers\BreweryController;
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/', [BreweryController::class, 'index'])->name('breweries.index');
     Route::get('/breweries', [BreweryController::class, 'index'])->name('breweries.index');
-    Route::get('/beers', [BeersController::class, 'index'])->name('beers.index');
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-
 
 require __DIR__.'/auth.php';
