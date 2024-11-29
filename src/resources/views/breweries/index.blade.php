3c1f85c85e34b1757c0b89df342c3dca4b241ba4
Marco Sala <sala.marco1984@gmail.com>
2024-11-29 14:29:15 +0100

    fix layout

--- src/resources/views/breweries/index.blade.php
+++ src/resources/views/breweries/index.blade.php
@@ -8,6 +8,19 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">   
+                <div class="flex justify-between items-center p-4">
+                    <div class="hidden sm:flex sm:items-center sm:justify-between">
+                        <div class="flex items-center">
+                            @for ($i = 1; $i <= $totalPages; $i++)
+                                <a href="{{ route('breweries.index', ['page' => $i]) }}" 
+                                   class="ml-2 @if ($i == $currentPage) text-red-600 font-bold @else text-gray-500 hover:text-gray-700 @endif">
+                                   {{ $i }}
+                                </a>
+                            @endfor
+                        </div>
+                    </div>
+                </div>
+                <hr/>
                 @foreach ($breweries as $brewery)
                     <div class="p-2 text-gray-900"> <!-- Padding ridotto a p-2 -->
                         {{ $brewery['name'] }} - {{ $brewery['city'] }}, {{ $brewery['state'] }}
